fea014beed6aed00e3b548f917b7e643034fba1e
debugger <parapet-origami.0o@icloud.com>
2023-08-07 17:41:06 +0900

    chap03_1
    챕터 03 첫번째 예제

---
+++ spring5_chap03/src/main/java/spring/MemberDao.java
@@ -0,0 +1,25 @@
+package spring;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemberDao {
+
+    private static long nextId = 0;
+
+    // Map의 key는 이메일, value는 멤버객체
+    private Map<String, Member> map = new HashMap<>();
+
+    public Member selectByEmail(String email){
+        return map.get(email);
+    }
+
+    public void insert(Member member){
+        member.setId(++nextId);
+        map.put(member.getEmail(), member);
+    }
+
+    public void update(Member member){
+        map.put(member.getEmail(), member);
+    }
+}
