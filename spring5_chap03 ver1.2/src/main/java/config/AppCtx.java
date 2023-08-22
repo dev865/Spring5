358b9bcd232d983ad70cbcfa3507a2b3dd2b13fa
debugger <parapet-origami.0o@icloud.com>
2023-08-22 17:10:44 +0900

    chap03 세터 메서드 DI방식
    84~92page

---
+++ spring5_chap03 ver1.2/src/main/java/config/AppCtx.java
@@ -0,0 +1,52 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import spring.*;
+
+@Configuration
+public class AppCtx {
+
+    @Bean
+    public MemberDao memberDao(){
+        return new MemberDao();
+    }
+
+    @Bean
+    public MemberRegisterService memberRegSvc(){
+        return new MemberRegisterService(memberDao());
+    }
+
+    @Bean
+    public ChangePasswordService changePwdSvc(){
+        ChangePasswordService pwdSvc = new ChangePasswordService();
+        pwdSvc.setMemberDao(memberDao());
+        return pwdSvc;
+    }
+
+    @Bean
+    public MemberPrinter memberPrinter(){
+        return new MemberPrinter();
+    }
+
+    @Bean
+    public MemberListPrinter listPrinter(){
+        return new MemberListPrinter(memberDao(), memberPrinter());
+    }
+
+    @Bean
+    public MemberInfoPrinter infoPrinter(){
+        MemberInfoPrinter infoPrinter = new MemberInfoPrinter();
+        infoPrinter.setPrinter(memberPrinter());
+        infoPrinter.setMemberDao(memberDao());
+        return infoPrinter;
+    }
+
+    @Bean
+    public VersionPrinter versionPrinter() {
+        VersionPrinter versionPrinter = new VersionPrinter();
+        versionPrinter.setMajorVersion(5);
+        versionPrinter.setMinorVersion(0);
+        return versionPrinter;
+    }
+}
